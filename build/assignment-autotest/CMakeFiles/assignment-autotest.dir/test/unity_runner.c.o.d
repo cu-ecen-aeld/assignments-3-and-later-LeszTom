5b566aab5ccbbc1c6c114ef460514f3534328c61
Leszek Tomaszewski <l.tomaszewski@wb.com.pl>
2025-08-09 22:19:07 +0200

    Assignment 3 part 2

--- build/assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d
+++
@@ -1,3 +0,0 @@
-assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o: \
- /home/ltomaszewski/Work_Place/Coursera/Linux_System_Programming_and_Introduction_to_Buildroot/assignment-3-part-1/assignment-autotest/test/unity_runner.c \
- /usr/include/stdc-predef.h
