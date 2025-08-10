c15047e63b2159956f6e5aa854c58d26c973c47c
Leszek Tomaszewski <l.tomaszewski@wb.com.pl>
2025-08-10 23:23:52 +0200

    assignment-3-part-2 commit 1

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 FILESDIR=$1
 SEARCHSTR=$2
 
-if [[ -z $FILESDIR || -z $SEARCHSTR ]];then
+if [ -z $FILESDIR || -z $SEARCHSTR ];then
     echo "filesdir or searchstr argument is missing"
     exit 1
 fi
 
-if [[ ! -d $FILESDIR ]];then
+if [ ! -d $FILESDIR ];then
     echo "$FILESDIR does not represent a directory on the filesystem"
     exit 1
 fi
@@ -19,7 +19,7 @@ X=0
 Y=0
 
 for file in $OUTPUT;do
-    if [[ -f $file ]];then
+    if [ -f $file ];then
         matching_lines=$(cat $file | grep $SEARCHSTR | wc -l)
         X=$(($X+1))
         Y=$(($Y+$matching_lines))
